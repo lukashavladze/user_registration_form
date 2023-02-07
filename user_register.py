cac68d4e9e71ffb9e82948fde15cff20586bb9c5
Luka <lukianeshavladze@gmail.com>
2023-02-07 14:49:58 +0400

    adding visuals 2

--- user_register.py
+++ user_register.py
@@ -3,20 +3,30 @@ import requests
 
 # making tkinter window
 window = tkinter.Tk()
-window.geometry("800x600")
+window.geometry("600x600")
 window.title("User Registration Form")
 window.config(bg="gray")
 
 
 # making Entries
-First_name = tkinter.Entry(window, background="white").place(x=130, y=50)
-Last_name = tkinter.Entry(window, background="white").place(x=130, y=100)
-Email = tkinter.Entry(window, background="white").place(x=130, y=150)
-Password = tkinter.Entry(window, background="white").place(x=130, y=200)
-Repeat_password = tkinter.Entry(window, background="white").place(x=130, y=250)
-Country = tkinter.Entry(window, background="white").place(x=130, y=300)
-City = tkinter.Entry(window, background="white").place(x=130, y=350)
-Adress = tkinter.Entry(window, background="white").place(x=130, y=400)
+First_name = tkinter.Entry(window, background="white")
+First_name.place(x=130, y=50)
+Last_name = tkinter.Entry(window, background="white")
+Last_name.place(x=130, y=100)
+Email = tkinter.Entry(window, background="white")
+Email.place(x=130, y=150)
+Password = tkinter.Entry(window, background="white")
+Password.place(x=130, y=200)
+Repeat_password = tkinter.Entry(window, background="white")
+Repeat_password.place(x=130, y=250)
+Country = tkinter.Entry(window, background="white")
+Country.place(x=130, y=300)
+City = tkinter.Entry(window, background="white")
+City.place(x=130, y=350)
+Adress = tkinter.Entry(window, background="white")
+Adress.place(x=130, y=400)
+
+
 
 # titles beside labels
 tkinter.Label(window, background="gray", padx=5, pady=6, text="First Name").place(x=25, y=45)
@@ -29,18 +39,34 @@ tkinter.Label(window, background="gray", padx=5, pady=6, text="City").place(x=25
 tkinter.Label(window, background="gray", padx=5, pady=6, text="Adress").place(x=25, y=395)
 
 
-# making buttons
-save_button = tkinter.Button(window, text="Register", width=10, background="green").place(x=200, y=500)
+
 
 def save():
     user_list = []
-    if password != Repeat_password:
-        return "password doesn't match"
-    elif Email in user_list:
-        return "this email already exists"
+    a = First_name.get()
+    b = Last_name.get()
+    c = Email.get()
+    d = Password.get()
+    e = Repeat_password.get()
+    f = Country.get()
+    g = City.get()
+    h = Adress.get()
+    print(c)
+    print(type(c))
+    if c in user_list:
+        print("this email already exists")
+    elif d != e:
+        print("password doesn't match")
     else:
-        user_list.append(list(First_name, Last_name, Email, Password, Country, City, Adress))
-        return "registered succesfully"
+        user_list.append(list([a, b, c, d, f, g, h]))
+        print(user_list)
+        print("registered succesfully")
+
+
+# making buttons
+save_button = tkinter.Button(window, text="Register", width=10, height=2, background="green", command=save)
+save_button.place(x=200, y=500)
+
 
 
 window.mainloop()
